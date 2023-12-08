35016de1facf47d9bd0e599f30b5dadf39244895
senanayake <janith@edhirya.com>
2023-12-08 14:31:45 +0530

    add test controller class

---
+++ src/main/java/com/developeriqtracker/contributormanagementservice/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.developeriqtracker.contributormanagementservice.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping("/")
+    public ResponseEntity<Map<String, String>> sayHello() {
+        Map<String, String> map = new HashMap<>();
+        map.put("response", "sucess");
+        return ResponseEntity.ok(map);
+    }
+}
