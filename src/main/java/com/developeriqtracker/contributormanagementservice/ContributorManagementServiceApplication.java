cef6b62cc69df1cb1bf89c077c0442df28007ac6
senanayake <janith@edhirya.com>
2023-12-08 13:54:12 +0530

    initial commit

---
+++ src/main/java/com/developeriqtracker/contributormanagementservice/ContributorManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.developeriqtracker.contributormanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContributorManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContributorManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/developeriqtracker/contributormanagementservice/ContributorManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developeriqtracker.contributormanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContributorManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
